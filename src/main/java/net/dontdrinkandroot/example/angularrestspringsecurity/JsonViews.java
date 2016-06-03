3c0daedc1f0a34a6b6df9966c8a557cec2a44837
zhimin711 <zhimin711@sina.com>
2016-06-03 10:19:29 +0800

    修改Mysql数据库

--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/JsonViews.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/JsonViews.java
@@ -1,14 +1,11 @@
 package net.dontdrinkandroot.example.angularrestspringsecurity;
 
-public class JsonViews
-{
+public class JsonViews {
 
-	public static class User
-	{
-	}
+    public static class User {
+    }
 
-	public static class Admin extends User
-	{
-	}
+    public static class Admin extends User {
+    }
 
 }
